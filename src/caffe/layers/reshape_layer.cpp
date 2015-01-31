23d5c03163e28f008a7b1613eaefa89806579cba
Jeff Donahue <jeff.donahue@gmail.com>
2015-01-30 23:19:34 -0800

    Add (very simple version of) ReshapeLayer

---
+++ src/caffe/layers/reshape_layer.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+#include "caffe/common_layers.hpp"
+#include "caffe/layer.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void ReshapeLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top) {
+  top[0]->Reshape(this->layer_param_.reshape_param().shape());
+  CHECK_EQ(top[0]->count(), bottom[0]->count())
+     << "new shape must have the same count as input";
+  top[0]->ShareData(*bottom[0]);
+  top[0]->ShareDiff(*bottom[0]);
+}
+
+INSTANTIATE_CLASS(ReshapeLayer);
+REGISTER_LAYER_CLASS(Reshape);
+
+}  // namespace caffe
