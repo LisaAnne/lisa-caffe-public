a3c400e0b497b7daf7a0edb83056b36c33688cef
Jeff Donahue <jeff.donahue@gmail.com>
2015-04-08 19:49:24 -0700

    make get_coco2014_aux.sh runnable from anywhere

--- data/coco/get_coco2014_aux.sh
+++ data/coco/get_coco2014_aux.sh
@@ -1,13 +1,23 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #
 # Downloads Andrej Karpathy's train/val/test splits of COCO2014 as text files.
 
+# change to directory $DIR where this script is stored
+pushd .
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+cd $DIR
+
+FILENAME=coco2014_aux.tar.gz
+
 echo "Downloading..."
 
-wget http://dl.caffe.berkeleyvision.org/coco2014_aux.tar.gz
+wget http://dl.caffe.berkeleyvision.org/$FILENAME
 
-echo "Unzipping..."
+echo "Unzipping to $DIR"
 
-tar -xf coco2014_aux.tar.gz && rm -f coco2014_aux.tar.gz
+tar -xf $FILENAME && rm -f $FILENAME
 
 echo "Done."
+
+# change back to original working directory
+popd
