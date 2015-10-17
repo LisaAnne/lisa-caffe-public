b1f5b18b30d00a14c00a1f7def56919a659fc689
LisaAnne <lisa.a.hendricks@gmail.com>
2015-10-16 17:36:29 -0700

    Changed init.py to init_example.py

--- examples/captions_add_new_word/init.py
+++
@@ -1,4 +0,0 @@
-#specifies all relative paths
-coco_root = '../../data/coco/coco/images/'
-imagenet_root = '/y/lisaanne/imageData/imagenet/'
-#imagenet_root = ''
---
+++ examples/captions_add_new_word/init_example.py
@@ -0,0 +1,3 @@
+#specifies all relative paths for images
+coco_root = '/your/path/to/coco/images'
+imagenet_root = '/your/path/to/imagenet/images'
