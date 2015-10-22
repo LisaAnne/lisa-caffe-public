568d53b5dcb2ea678ccef02036b2de8eb611c3dc
LisaAnne <lisa.a.hendricks@gmail.com>
2015-10-21 19:55:31 -0700

    Added to init_examples

--- examples/captions_add_new_word/init_example.py
+++ examples/captions_add_new_word/init_example.py
@@ -1,3 +1,11 @@
-#specifies all relative paths for images
-coco_root = '/your/path/to/coco/images'
-imagenet_root = '/your/path/to/imagenet/images'
+caffe_dir = '/home/lisa/caffe-LSTM-video/'
+coco_data_dir = '/home/lisa/caffe-LSTM-video/data/coco/'
+
+pretrained_lm = '/z/lisaanne/pretrained_lm/'
+
+#specifies all relative paths
+coco_root = '../../data/coco/coco/images/'
+imagenet_root = '/z/lisaanne/imageData/imagenet/'
+
+
+
