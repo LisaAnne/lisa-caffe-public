96139099d3f7d180d5a1883572fe914cb52d802c
LisaAnne <lisa.a.hendricks@gmail.com>
2015-10-28 17:34:52 -0700

    Updating transfer_weights.py

--- examples/captions_add_new_word/transfer_weights.py
+++ examples/captions_add_new_word/transfer_weights.py
@@ -10,44 +10,37 @@ import hickle as hkl
 from nltk.corpus import wordnet as wn
 #import find_close_words
 
-save_tag_template = 'closest_W2V_%s_test'
+save_tag_template = 'closestFirst_W2V_%s'
 transfer_embed = False
 num_close_words_im = 1
 num_close_words_lm = 1
 
+separate_files = True #This is kind of confusing, but if this flag is true, it will only transfer the second closest weight
+
 all_add_words = []
 
 #zebra
-#add_words = {'words': ['zebra', 'zebras'], 'classifiers': ['zebra', 'zebra'], 'illegal_words': ['zebra']}
-#all_add_words.append(add_words)
-#add_words = {'words': ['pizza', 'pizzas'], 'classifiers': ['pizza', 'pizza'], 'illegal_words': ['pizza']}
-#all_add_words.append(add_words)
-#add_words = {'words': ['suitcase', 'suitcases', 'luggage', 'luggages'], 'classifiers': ['suitcase', 'suitcase', 'luggage', 'luggage'], 'illegal_words': ['luggage', 'suitcase']}
-#all_add_words.append(add_words)
-#add_words = {'words': ['bottle', 'bottles'], 'classifiers': ['bottle', 'bottle'], 'illegal_words': ['bottle']}
-#all_add_words.append(add_words)
+add_words = {'words': ['zebra', 'zebras'], 'classifiers': ['zebra', 'zebra'], 'illegal_words': ['zebra']}
+all_add_words.append(add_words)
+add_words = {'words': ['pizza', 'pizzas'], 'classifiers': ['pizza', 'pizza'], 'illegal_words': ['pizza']}
+all_add_words.append(add_words)
+add_words = {'words': ['suitcase', 'suitcases', 'luggage', 'luggages'], 'classifiers': ['suitcase', 'suitcase', 'luggage', 'luggage'], 'illegal_words': ['luggage', 'suitcase']}
+all_add_words.append(add_words)
+add_words = {'words': ['bottle', 'bottles'], 'classifiers': ['bottle', 'bottle'], 'illegal_words': ['bottle']}
+all_add_words.append(add_words)
 add_words = {'words': ['bus', 'busses'], 'classifiers': ['bus', 'bus'], 'illegal_words': ['bus']}
 all_add_words.append(add_words)
-#add_words = {'words': ['couch', 'couches'], 'classifiers': ['couch', 'couch'], 'illegal_words': ['couch']}
-#all_add_words.append(add_words)
-#add_words = {'words': ['microwave', 'microwaves'], 'classifiers': ['microwave', 'microwave'], 'illegal_words': ['microwave']}
-#all_add_words.append(add_words)
-#add_words = {'words': ['racket', 'rackets', 'racquet', 'racquets'], 'classifiers': ['racket', 'racket', 'racquet', 'racquet'], 'illegal_words': ['racket', 'racquet']}
-#all_add_words.append(add_words)
+add_words = {'words': ['couch', 'couches'], 'classifiers': ['couch', 'couch'], 'illegal_words': ['couch']}
+all_add_words.append(add_words)
+add_words = {'words': ['microwave', 'microwaves'], 'classifiers': ['microwave', 'microwave'], 'illegal_words': ['microwave']}
+all_add_words.append(add_words)
+add_words = {'words': ['racket', 'rackets', 'racquet', 'racquets'], 'classifiers': ['racket', 'racket', 'racquet', 'racquet'], 'illegal_words': ['racket', 'racquet']}
+all_add_words.append(add_words)
 
 
 #model_weights = '/z/lisaanne/snapshots_caption_models/attributes_JJ100_NN300_VB100_zebra_cocoImages_captions_fixLMPretrain_iter_110000'
 
 
-if 'predict-lm' in net.params.keys():
-  predict_lm = 'predict-lm'
-else:
-  predict_lm = 'predict'
-
-if len(net.params['predict-im']) > 1:
-  im_bias = True
-else: 
-  im_bias = False
 
 attributes = pkl.load(open('../coco_attribute/attribute_lists/attributes_JJ100_NN300_VB100.pkl','rb'))
 
@@ -102,6 +95,15 @@ for add_words in all_add_words:
   model_weights = '/y/lisaanne/mrnn_direct/snapshots/attributes_JJ100_NN300_VB100_eightClusters_cocoImages_captions_fixLMPretrain_fixSplit5_iter_110000'
   model='mrnn_attributes_fc8.direct.from_features.wtd.prototxt'
   net = caffe.Net(model, model_weights + '.caffemodel', caffe.TRAIN)
+  if 'predict-lm' in net.params.keys():
+    predict_lm = 'predict-lm'
+  else:
+    predict_lm = 'predict'
+  
+  if len(net.params['predict-im']) > 1:
+    im_bias = True
+  else: 
+    im_bias = False
   save_tag = save_tag_template % add_words['words'][0]
   for aw, word in enumerate(add_words['words']):
     close_words_im[word] = {}
@@ -112,19 +114,27 @@ for add_words in all_add_words:
   
     close_words_im[word] = {}
     close_words_lm[word] = {}
-  
-    close_words_im[word]['close_words'] = [attributes[i] for i in np.argsort(word_sims)[-num_close_words_im:]]
-    close_words_lm[word]['close_words'] = [attributes[i] for i in np.argsort(word_sims)[-num_close_words_lm:]]
-    close_words_im[word]['weights'] = [1./num_close_words_im]*num_close_words_im
-    close_words_lm[word]['weights'] = [1./num_close_words_lm]*num_close_words_im
+ 
+    if not separate_files:  
+      close_words_im[word]['close_words'] = [attributes[i] for i in np.argsort(word_sims)[-num_close_words_im:]]
+      close_words_lm[word]['close_words'] = [attributes[i] for i in np.argsort(word_sims)[-num_close_words_lm:]]
+      close_words_im[word]['weights'] = [1./num_close_words_im]*num_close_words_im
+      close_words_lm[word]['weights'] = [1./num_close_words_lm]*num_close_words_im
+
+    else:
+      close_words_im[word]['close_words'] = [attributes[np.argsort(word_sims)[-num_close_words_im]]]
+      close_words_lm[word]['close_words'] = [attributes[np.argsort(word_sims)[-num_close_words_lm]]]
+      close_words_im[word]['weights'] = [1.]
+      close_words_lm[word]['weights'] = [1.]
+      
    
   vocab_file = '../coco_caption/h5_data/buffer_100/vocabulary.txt'
   vocab_lines = open(vocab_file, 'rb').readlines()
   vocab_lines = [v.strip() for v in vocab_lines]
   vocab_lines = ['<EOS>'] + vocab_lines
   
-  predict_weights_lm = copy.deepcopy(net.params['predict-lm'][0].data)
-  predict_bias_lm = copy.deepcopy(net.params['predict-lm'][1].data)
+  predict_weights_lm = copy.deepcopy(net.params[predict_lm][0].data)
+  predict_bias_lm = copy.deepcopy(net.params[predict_lm][1].data)
   predict_weights_im = copy.deepcopy(net.params['predict-im'][0].data)
   #predict_bias_im = copy.deepcopy(net.params['predict-im'][1].data)
   
@@ -169,8 +179,8 @@ for add_words in all_add_words:
       close_word_idx = vocab_lines.index(close_word)
       predict_weights_im[close_word_idx,attribute_loc] = 0 
      
-  net.params['predict-lm'][0].data[...] = predict_weights_lm
-  net.params['predict-lm'][1].data[...] = predict_bias_lm
+  net.params[predict_lm][0].data[...] = predict_weights_lm
+  net.params[predict_lm][1].data[...] = predict_bias_lm
   net.params['predict-im'][0].data[...] = predict_weights_im
   #net.params['predict-im'][1].data[...] = predict_bias_im
   net.save('%s.%s.caffemodel' %(model_weights, save_tag))
