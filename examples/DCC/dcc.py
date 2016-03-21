4d9da924f659e6b717b21889dcdb97bbe3e26139
LisaAnne <lisa.a.hendricks@gmail.com>
2016-03-21 16:25:39 -0700

    Started DCC file on durian3

--- examples/DCC/dcc.py
+++ examples/DCC/dcc.py
@@ -1,17 +1,30 @@
 import sys
 from train_lexical import eval_classifiers 
+from train_captions import transfer_weights
 import argparse
 import pdb 
 
 def extract_features(args):
   eval_classifiers.extract_features(args.model, args.model_weights, args.imagenet_images, args.device, args.image_dim, args.lexical_feature, args.batch_size)
 
+def transfer(args):
+  
+  transfer_weights.transfer_net(args.model, args.model_weights, args.orig_attributes, args.all_attributes, args.vocab)
+  eval('transfer_weights.' + args.transfer_type)(args.words, args.classifiers, ags.closeness_metric, args.log) 
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--model",type=str)
   parser.add_argument("--model_weights",type=str)
-  parser.add_argument("--imagenet_images",type=str)
-  parser.add_argument("--lexical_feature",type=str, default='probs')
+  parser.add_argument("--imagenet_images",type=str) #extract_features
+  parser.add_argument("--lexical_feature",type=str, default='probs') #name of layer to extract
+  parser.add_argument("--orig_attributes",type=str, default='')
+  parser.add_argument("--all_attributes",type=str, default='')
+  parser.add_argument("--vocab", type=str, default='')
+  parser.add_argument("--words", type=str, defulat='')
+  parser.add_argument("--classifiers", type=str, default='') #list of classifiers
+  parser.add_argument("--closeness_metric", type=str, default='closeness_embedding')
+  parser.add_argument("--transfer_type", type=str, default='direct_transfer')
 
   parser.add_argument("--device",type=int, default=0)
   parser.add_argument("--image_dim",type=int, default=227)
@@ -19,12 +32,17 @@ if __name__ == "__main__":
 
   parser.add_argument('--extract_features', dest='extract_features', action='store_true')
   parser.set_defaults(extract_features=False)
+  parser.add_argument('--transfer', dest='transfer', action='store_true')
+  parser.set_defaults(transfer=False)
+  parser.add_argument('--log', dest='log', action='store_true')
+  parser.set_defaults(log=False)
 
   args = parser.parse_args()
   
   if args.extract_features:
     extract_features(args) 
 
-
+  if args.transfer:
+    transfer(args)
 
 
--- examples/DCC/utils/config.py
+++ examples/DCC/utils/config.py
@@ -1,1 +1,3 @@
+
+
 coco_template = 'coco_images/%s2014/COCO_%s2014_%012d.jpg'
