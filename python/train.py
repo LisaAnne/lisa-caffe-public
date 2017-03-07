ca20d364c9fbc6a093f9a2a9cd1139fc458403e3
LisaAnne <lisa.a.hendricks@gmail.com>
2017-03-06 22:50:58 -0800

    made train executable

--- python/train.py
+++ python/train.py
@@ -6,18 +6,27 @@ sys.path.append('./')
 import caffe
 caffe.set_mode_gpu()
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--solver", type=str, default=None)
-parser.add_argument("--gpu", type=int, default=0)
-parser.add_argument("--weights", type=str, default=None)
-args = parser.parse_args()
+def train_caffe_model(solver, weights=None, gpu=0, add_paths=[]):
 
-if not args.solver:
-  raise Exception("Must indicate solver")
+  for add_path in add_paths:
+    sys.path.append(add_path)
 
-caffe.set_device(args.gpu)
-solver = caffe.get_solver(args.solver)
-if args.weights:
-  solver.net.copy_from(args.weights)
-  print("Copying weights from %s" %args.weights)
-solver.solve()
+  caffe.set_device(gpu)
+  solver = caffe.get_solver(solver)
+  if weights:
+    solver.net.copy_from(weights)
+    print("Copying weights from %s" %weights)
+  solver.solve()
+
+if __name__ == "__main__":
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--solver", type=str, default=None)
+  parser.add_argument("--gpu", type=int, default=0)
+  parser.add_argument("--weights", type=str, default=None)
+  args = parser.parse_args()
+  
+  if not args.solver:
+    raise Exception("Must indicate solver")
+
+  train_caffe_model(args.solver, args.weights, args.gpu) 
